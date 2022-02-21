b91a261f653f8bfbd9e4579707ef6820b3aa48eb
Dilshan Udawaththa <d.udawaththa@sana-commerce.com>
2022-02-22 04:11:02 +0530

    remove additional pages

--- src/components/PrizingBox.js
+++
@@ -1,9 +0,0 @@
-const PrizingBox = (props) => {
-    return(
-        <>
-            <span className="item_price">{props.currencySymbol}{props.priceBegin}<sup>{props.priceLast}</sup></span>
-        </>
-    );
-}
-
-export default PrizingBox;
--- src/components/ProductImage.js
+++
@@ -1,9 +0,0 @@
-const ProductImage = (props) => {
-    return(
-        <>
-            <img src={props.image} alt="productImage.jpg"/>
-        </>
-    );
-}
-
-export default ProductImage;
